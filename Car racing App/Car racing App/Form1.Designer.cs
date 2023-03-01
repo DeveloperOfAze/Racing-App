f7d45e66fc7f6f667dbb85218a777445c7b5f6c7
Ehed <islamzade.ehed@gmail.com>
2023-03-01 16:29:58 +0400

    Racing

--- Car racing App/Car racing App/Form1.Designer.cs
+++ Car racing App/Car racing App/Form1.Designer.cs
@@ -34,16 +34,25 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -60,7 +69,7 @@
             this.label5.BackColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(-1, 124);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(1145, 20);
+            this.label5.Size = new System.Drawing.Size(986, 19);
             this.label5.TabIndex = 5;
             // 
             // label3
@@ -68,7 +77,7 @@
             this.label3.BackColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(-1, 266);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(1145, 17);
+            this.label3.Size = new System.Drawing.Size(973, 17);
             this.label3.TabIndex = 6;
             // 
             // label2
@@ -76,28 +85,16 @@
             this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(-1, 445);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1145, 17);
+            this.label2.Size = new System.Drawing.Size(973, 35);
             this.label2.TabIndex = 7;
-            // 
-            // label4
-            // 
-            this.label4.BackColor = System.Drawing.Color.White;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(1141, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(105, 462);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "F\r\nI\r\nN\r\nI\r\nS\r\nH\r\n";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Black;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 21);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 21);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(201, 100);
+            this.pictureBox1.Size = new System.Drawing.Size(117, 100);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
@@ -106,7 +103,7 @@
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(-1, 146);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(201, 117);
+            this.pictureBox2.Size = new System.Drawing.Size(185, 117);
             this.pictureBox2.TabIndex = 10;
             this.pictureBox2.TabStop = false;
             // 
@@ -115,7 +112,7 @@
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
             this.pictureBox3.Location = new System.Drawing.Point(-1, 286);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(223, 152);
+            this.pictureBox3.Size = new System.Drawing.Size(185, 152);
             this.pictureBox3.TabIndex = 11;
             this.pictureBox3.TabStop = false;
             // 
@@ -138,25 +135,120 @@
             // 
             // label6
             // 
-            this.label6.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Font = new System.Drawing.Font("Century Schoolbook", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label6.Location = new System.Drawing.Point(249, 496);
+            this.label6.Location = new System.Drawing.Point(220, 498);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(987, 47);
+            this.label6.Size = new System.Drawing.Size(780, 47);
             this.label6.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Font = new System.Drawing.Font("Showcard Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(970, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(184, 462);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "F\r\nI\r\nN\r\nI\r\nS\r\nH\r\n";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(1145, 574);
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(425, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "ADINIZ:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(425, 227);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "SOYADINIZ:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(609, 183);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 34);
+            this.textBox1.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(609, 224);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 36);
+            this.textBox2.TabIndex = 20;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "At",
+            "Canavar",
+            "It"});
+            this.comboBox1.Location = new System.Drawing.Point(609, 266);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(419, 269);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 20);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "TEXMININIZ:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(506, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Tesdiq Et";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1248, 568);
+            this.ClientSize = new System.Drawing.Size(1144, 568);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label5);
@@ -167,7 +259,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -177,12 +271,20 @@
         private Label label5;
         private Label label3;
         private Label label2;
-        private Label label4;
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
         private Button button1;
         private System.Windows.Forms.Timer timer1;
         private Label label6;
+        private Label label7;
+        private PictureBox pictureBox4;
+        private Label label4;
+        private Label label8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ComboBox comboBox1;
+        private Label label9;
+        private Button button2;
     }
 }
--- Car racing App/Car racing App/Form1.cs
+++ Car racing App/Car racing App/Form1.cs
@@ -1,6 +1,9 @@
+using System.Media;
+
 namespace Car_racing_App
 {
     public partial class Form1 : Form
+
     {
         public Form1()
         {
@@ -10,44 +13,73 @@ namespace Car_racing_App
         Random rnd = new Random();
         private void label1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             timer1.Enabled=true;
+      
+            
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Visible = false;
+            textBox1.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label4.Visible= false;
+            pictureBox4.Visible=false;
+            comboBox1.Visible = false;
+            button2.Visible = false;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            int widthHorse = pictureBox1.Left;
-            int widthRabbit = pictureBox2.Left;
-            int widthDog=pictureBox3.Left;
-            int start = label4.Left;
-            pictureBox1.Left = pictureBox1.Left + rnd.Next(5, 16);
-            pictureBox2.Left = pictureBox2.Left + rnd.Next(5, 16);
-            pictureBox3.Left = pictureBox3.Left + rnd.Next(5, 16);
+            string texmin = comboBox1.Text;
+            string ad = textBox1.Text;
+            string soyad = textBox2.Text;
+            int widthHorse = pictureBox1.Width;
+            int widthRabbit = pictureBox2.Width;
+            int widthDog=pictureBox3.Width;
+            int start = label7.Left;
+            pictureBox1.Left = pictureBox1.Left + rnd.Next(0, 5);
+            pictureBox2.Left = pictureBox2.Left + rnd.Next(0, 5);
+            pictureBox3.Left = pictureBox3.Left + rnd.Next(0, 5);
 
-            if (pictureBox1.Left > pictureBox2.Left+5 && pictureBox1.Left > pictureBox3.Left + 5)
+            if (pictureBox1.Left > pictureBox2.Left + 5 && pictureBox1.Left > pictureBox3.Left + 5)
             {
                 label6.Text = "At Birincidir";
             }
 
-            if (pictureBox2.Left > pictureBox1.Left+5 && pictureBox2.Left > pictureBox3.Left + 5)
+            if (pictureBox2.Left > pictureBox1.Left + 5 && pictureBox2.Left > pictureBox3.Left + 5)
             {
                 label6.Text = "Canavar Irelileyir";
             }
 
 
-            if (pictureBox3.Left > pictureBox1.Left + 5 && pictureBox3.Left > pictureBox2.Left + 5)
+            if (pictureBox2.Left > pictureBox1.Left + 5 && pictureBox2.Left > pictureBox3.Left + 5)
             {
                 label6.Text = "It birinci oldu";
             }
 
             if (widthHorse + pictureBox1.Left >= start)
-            {
+            {  
                 timer1.Enabled = false;
                 label6.Text = "At Yarisi Qazandi";
+            }
+            if (widthHorse + pictureBox1.Left >= start && texmin == "At")
+            {
+
+                MessageBox.Show(ad + soyad + "Siz Qazandiniz");
+            }
+            if (label6.Text == "At Yarisi Qazandi" && texmin != "At")
+            {
+                MessageBox.Show(ad + soyad + "Siz Qazanmadiniz");
+
             }
             if (widthRabbit + pictureBox2.Left >= start)
             {
@@ -55,10 +87,28 @@ namespace Car_racing_App
                 label6.Text = "Canavar Yarisi Qazandi";
             }
 
+            if (widthRabbit + pictureBox2.Left >= start &&  texmin == "Canavar")
+            {
+                MessageBox.Show(ad + soyad + "Siz Qazandiniz");
+            }
+            if (label6.Text == "Canavar Yarisi Qazandi" && texmin != "Canavar")
+            {
+                MessageBox.Show(ad + soyad + "Siz Qazanmadiniz");
+
+            }
             if (widthDog + pictureBox3.Left >= start)
             {
                 timer1.Enabled = false;
                 label6.Text = "It Yarisi Qazandi";
+            }
+            if (widthDog + pictureBox3.Left >= start && texmin == "It" )
+            {
+                MessageBox.Show(ad + soyad + "Siz Qazandiniz");
+            }
+            if(label6.Text== "It Yarisi Qazandi" && texmin != "It")
+            {
+                MessageBox.Show(ad + soyad + "Siz Qazanmadiniz");
+
             }
         }
 
@@ -67,11 +117,14 @@ namespace Car_racing_App
             LeftHorse = pictureBox1.Left;
             LeftRabbit =pictureBox2.Left;
             LeftDog = pictureBox3.Left; 
+
+
+                    
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\Ehed\Downloads\electronic-future-beats-117997.wav");
+            simpleSound.Play();
+        
         }
 
-        private void label4_Click(object sender, EventArgs e)
-        {
 
-        }
     }
 }
